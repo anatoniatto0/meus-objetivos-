b05c61e298b9b42af0640f8cfbfedd9b0fc6b44c
anatoniatto0 <110242868+anatoniatto0@users.noreply.github.com>
2024-03-06 09:59:44 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes)
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0;i <botoes.length;i++){ 
+   botoes[i].onclick = function(){
+
+    for(let j=0;j<botoes.length;j++){
+        botoes[j].classList.remove("ativo")
+        textos[j].classList.remove("ativo");
+    }
+
+    botoes[j].classList.add("ativo");
+    textos[i].classList.add("ativo");
+
+   }
+   
+   
+    console.log(i);
+  }
+  
+
